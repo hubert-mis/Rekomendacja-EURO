4cc6c636fa5509b152a9e6a1cec1f76fd8571df6
Hubert <hubert.k.mis@gmail.com>
2018-04-05 12:09:28 +0200

    Wczytywanie z World Bank i OECD

--- wczyt_danych.R
+++ wczyt_danych.R
@@ -10,3 +10,19 @@ gdp <- read.csv("U:/Informatyka Ekonomiczna/Projekt/pkb.csv")
 gdp <- gdp[-(3:5)]
 colnames(gdp)[2] <- "Gross Domestic Product"
 
+
+################## 
+library(RJSONIO)
+library(WDI)
+
+WDI(country = "PL", indicator = "NY.GDP.MKTP.CD", end = 2018, start = 2010)
+
+library(OECD)
+
+dataset <- "DP_LIVE"
+datastruc <- get_data_structure(dataset)
+str(a2, max.level = 1)
+datastruc$TIME
+
+
+temp <- get_dataset(dataset = dataset, filter = list(c("POL", "EA19"), c("CPI"), c("TOT")))
